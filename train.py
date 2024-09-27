079fea1b66d0ee8af1b8764e9bc83004aa539ea9
danilowi <danilowi@agh.edu.pl>
2024-09-27 11:55:54 +0200

    first train script

--- train.py
+++ train.py
@@ -3,8 +3,7 @@ import cv2
 
 
 model = YOLO('ultralytics/cfg/models/v8/quantyolov8.yaml').load('yolov8n.pt')
-model.train(data="coco8.yaml", epochs=3)  # train the model
+
+model.train(data="coco.yaml", name="first_quantyolov8", epochs=200, device=[0, 1], cache=True, plots=True, save_period=10)  # train the model
+# model.train(resume=True)
 # metrics = model.val(data="coco.yaml")  # evaluate model performance on the validation set
-results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-img = results[0].plot()
-cv2.imwrite('./trainresult.jpg', img)
