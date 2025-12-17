7e5a1144719b92a105d6d57e9b24e503c86a6c97
Michal <danilowi@agh.edu.pl>
2025-12-17 14:33:51 +0100

    plots lr every epoch, yolov8 configs added

--- ultralytics/utils/plotting.py
+++ ultralytics/utils/plotting.py
@@ -873,8 +873,7 @@ def plot_results(file: str = "path/to/results.csv", dir: str = "", on_plot: Call
     save_dir = Path(file).parent if file else Path(dir)
     files = list(save_dir.glob("results*.csv"))
     assert len(files), f"No results.csv files found in {save_dir.resolve()}, nothing to plot."
-
-    loss_keys, metric_keys = [], []
+    loss_keys, metric_keys, lr_keys = [], [], []
     for i, f in enumerate(files):
         try:
             data = pl.read_csv(f, infer_schema_length=None)
@@ -884,14 +883,16 @@ def plot_results(file: str = "path/to/results.csv", dir: str = "", on_plot: Call
                         loss_keys.append(c)
                     elif "metric" in c:
                         metric_keys.append(c)
+                    elif "lr" in c:
+                        lr_keys.append(c)
                 loss_mid, metric_mid = len(loss_keys) // 2, len(metric_keys) // 2
                 columns = (
                     loss_keys[:loss_mid] + metric_keys[:metric_mid] + loss_keys[loss_mid:] + metric_keys[metric_mid:]
                 )
-                fig, ax = plt.subplots(2, len(columns) // 2, figsize=(len(columns) + 2, 6), tight_layout=True)
+                fig, ax = plt.subplots(3, len(columns) // 2, figsize=(len(columns) + 2, 6), tight_layout=True)
                 ax = ax.ravel()
             x = data.select(data.columns[0]).to_numpy().flatten()
-            for i, j in enumerate(columns):
+            for i, j in enumerate(columns + lr_keys):
                 y = data.select(j).to_numpy().flatten().astype("float")
                 ax[i].plot(x, y, marker=".", label=f.stem, linewidth=2, markersize=8)  # actual results
                 ax[i].plot(x, gaussian_filter1d(y, sigma=3), ":", label="smooth", linewidth=2)  # smoothing line
